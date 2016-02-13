6e8e296d5ba219e3fc0186372478cc48ae7aacab
Miquel Camps Orteza <gafeman@gmail.com>
2016-02-13 11:22:11 +0100

    v1

---
+++ background.js
@@ -0,0 +1,31 @@
+var url = document.location.href;
+var zone = false;
+var found = false;
+var i = 0;
+
+while( i < pro_urls.length && !found ){
+	if( url.indexOf( pro_urls[i] ) > -1 ){
+		zone = 'pro';
+		found = true;
+	}
+	i++;
+}
+
+i = 0;
+while( i < dev_urls.length && !found ){
+	if( url.indexOf( dev_urls[i] ) > -1 ){
+		zone = 'dev';
+		found = true;
+	}
+	i++;
+}
+
+if( zone ){
+	var bar = document.createElement( 'div' );
+	bar.className = 'devspoiler_bar devspoiler_' + zone;
+	
+	var text = document.createTextNode("estás en " + zone);
+	bar.appendChild(text); 
+	
+	document.getElementsByTagName('body')[0].appendChild(bar); 
+}
---
+++ config.js
@@ -0,0 +1,2 @@
+var pro_urls = ["11.11.11.11", "domain1.com", "domain1.com/subfolder"];
+var dev_urls = ["localhost/phpMyAdmin", "22.22.22.22", "domain2.com", "domain2.com/subfolder"];
